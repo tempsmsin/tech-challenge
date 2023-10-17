5c2bbdd08b429046f894d3cd1c0515d45de2a2e6
tempsmsin <48242950+tempsmsin@users.noreply.github.com>
2023-10-17 15:18:50 +0530

    Update challenge 3.ps1

--- challenge3/challenge 3.ps1
+++ challenge3/challenge 3.ps1
@@ -5,8 +5,36 @@
 #object = {“a”:{“b”:{“c”:”d”}}}
 #key = a/b/c
 
+
+
 object = {“a”:{“b”:{“c”:”d”}}}
 key = a/b/c
+
+function Get-NestedValue {
+    param (
+        [Parameter(Mandatory=$true)]
+        [System.Collections.Hashtable] $object,
+        
+        [Parameter(Mandatory=$true)]
+        [string] $key
+    )
+
+    $keys = $key -split '/'
+    $currentObject = $object
+
+    foreach ($k in $keys) {
+        if ($currentObject.ContainsKey($k)) {
+            $currentObject = $currentObject[$k]
+        } else {
+            Write-Host "Key '$key' not found in the object."
+            return $null
+        }
+    }
+
+    return $currentObject
+}
+
+
 $value1 = Get-NestedValue -object $object -key $key
 Write-Host "Value 1: $value1"
 
